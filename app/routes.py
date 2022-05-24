6d6ab7550018cc9603b528662fc730be161ec145
Rubie <rubiedeoliveira@gmail.com>
2022-05-24 10:37:14 +0200

    update tags route to use json instead of form attribute

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,10 @@ from app import app
 
 @app.route('/tags', methods=['POST'])
 def tags():
-    if "question" not in request.form.keys():
+    if "question" not in request.json.keys():
         return "Bad request, please specify question to predict in body.\n" \
                "Expected format: { 'question': 'your question' }", 400
-    question = request.form['question']
+    question = request.json['question']
     print(question)
     return "OK"
 
